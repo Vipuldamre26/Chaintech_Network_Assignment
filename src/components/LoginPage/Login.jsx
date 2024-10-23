f79b3667b4a44ed752ed7f3b93c3626dd40fcce7
Vipul <vipuldamrepro26@gmail.com>
2024-10-23 16:49:01 +0530

    working on project started

---
+++ src/components/LoginPage/Login.jsx
@@ -0,0 +1,12 @@
+import './login.css';
+
+const Login = () => {
+    return(
+        <div className="login">
+            <h1>hello from login page</h1>
+        </div>
+    )
+}
+
+
+export default LoginPage
